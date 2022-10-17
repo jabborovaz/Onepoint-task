403eb195f4d37961a66cbd65752742e0acf1d71a
jabborovaz <greatman-98@mail.ru>
2022-10-17 17:49:46 +0500

    change routes background

--- src/container/Main.js
+++ src/container/Main.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 import { GrHome } from "react-icons/gr";
 import logo from "../images/onpoint.PNG";
 
 function Main() {
+  let location = useLocation();
+  console.log(location.pathname);
   return (
     <>
-      <div className="background-gradient h-screen flex flex-col justify-between">
+      <div
+        className={
+          location.pathname == "/"
+            ? "background-gradient h-screen flex flex-col justify-between"
+            : "first-slider-gradient h-screen flex flex-col justify-between"
+        }
+      >
         <div className="flex flex-row items-center space-x-4 px-16 py-4">
           <div className="pr-4 border-r-2 text-2xl m-0 border-r-black">
             <Link to="/">
